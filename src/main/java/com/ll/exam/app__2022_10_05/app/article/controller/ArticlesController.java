63a4e103d1d023ed9f5ac7abee0202ff85c15ee4
jeonga <jjeong0106@gmail.com>
2022-10-09 15:32:36 +0900

    GET /article/1 구현

--- src/main/java/com/ll/exam/app__2022_10_05/app/article/controller/ArticlesController.java
+++ src/main/java/com/ll/exam/app__2022_10_05/app/article/controller/ArticlesController.java
@@ -8,11 +8,11 @@ import com.ll.exam.app__2022_10_05.util.Util;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.Scanner;
 
 @RestController
 @RequestMapping("/articles")
@@ -29,6 +29,28 @@ public class ArticlesController  {
                 RsData.successOf(
                         Util.mapOf(
                                 "articles", articles
+                        )
+                )
+        );
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<RsData> detail(@PathVariable Long id) {
+        Article article = articleService.findById(id).orElse(null);
+
+        if (article == null) {
+            return Util.spring.responseEntityOf(
+                    RsData.of(
+                            "F-1",
+                            "해당 게시물은 존재하지 않습니다."
+                    )
+            );
+        }
+
+        return Util.spring.responseEntityOf(
+                RsData.successOf(
+                        Util.mapOf(
+                                "article", article
                         )
                 )
         );
